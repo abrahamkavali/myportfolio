df13037d4d97239ec502bd7b75a7792012877734
abrahamkavali <kavaliabraham09@gmail.com>
2025-05-08 21:40:36 -0500

    Set up GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
   base: "/myportfolio/",
+  plugins: [react(), tailwindcss()],
   server: { 
     port: 4000,
   },
